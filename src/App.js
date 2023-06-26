ca3794fe0e8db310c0a1ffdb09a8df1e092821eb
Trang Tran <trantrang181197@gmail.com>
2023-06-26 16:14:17 -0700

    modified App.js and added skeleton files for components and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Board from './pages/Board';
+import Card from './pages/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={ <Board/> } />
+        <Route path="card" element={ <Card/> } />
+      </Routes>
     </div>
   );
 }
